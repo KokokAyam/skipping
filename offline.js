ffaacc0ec950d1e5eee945e83541327d0e91a29b
KokokAyam <75647574+KokokAyam@users.noreply.github.com>
2021-04-20 22:47:40 +0700

    initial commit

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1618933615,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/tiledbackground.png",
+		"images/hati-sheet0.png",
+		"images/hati-sheet1.png",
+		"images/lantai-sheet0.png",
+		"images/tali-sheet0.png",
+		"images/tali-sheet1.png",
+		"images/tali-sheet2.png",
+		"images/tali-sheet3.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
